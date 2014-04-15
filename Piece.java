c91cc1d626a3b67f0cc73b3dbbdcddb3151fa5dd
apcs <apcs@mvhs-imacu52828.dcsdk12.org>
2014-04-15 08:22:29 -0600

    Piece

---
+++ Piece.java
@@ -0,0 +1,19 @@
+public class Piece {
+
+	public Piece(Space s, Boolean isBlack) {
+		Space s = this.s;
+		Boolean isBlack = this.isBlack;
+	}
+
+	public void setSpace(Space sNew) {
+		this.s = sNew;
+	}
+
+	public Space getSpace() {
+		return this.s;
+	}
+
+	public Boolean getTeam() {
+		return this.isBlack;
+	}
+}

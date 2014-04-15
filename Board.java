4afd0f22a76f4342753d9c3ae1d55c479b44db28
josh sparks <j80mar4@gmail.com>
2014-04-15 08:49:26 -0600

    added panel and board

---
+++ Board.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+
+import javax.swing.JPanel;
+
+public class Board {
+
+	public Board(){
+		init();
+	}	
+
+public void init(){
+
+}
+
+public void update(){
+
+}
+
+public void draw(Graphics2D g){
+	g.drawString("this works", 100, 100);
+}
+
+}

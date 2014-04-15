f8c718e537225f499a38289d0b08c841ad460de8
apcs <apcs@mvhs-imacu52828.dcsdk12.org>
2014-04-15 08:31:04 -0600

    Space

---
+++ Space.java
@@ -0,0 +1,23 @@
+public class Space {
+
+	public Space(int row, int col) {
+		int row = this.row;
+		int col = this.col;
+	}
+
+	public int getRow() {
+		return this.row;
+	}
+
+	public int getCol() {
+		return this.col;
+	}
+
+	public void setRow(int rowNew) {
+		row = rowNew;
+	}
+
+	public void setCol(int colNew) {
+		col = colNew;
+	}
+}

ed658b50652f8c6db92e15c0aa156b9161bc0694
apcs <apcs@mvhs-imacu52828.dcsdk12.org>
2014-04-17 08:33:41 -0600

    Added assorted methods

--- Space.java
+++ Space.java
@@ -1,8 +1,13 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+
 public class Space {
 
 	private int x, y;
+	private int row, col;
 
-	public Space(int row, int col) {
+	public Space(int col, int row) {
 		int row = this.row;
 		int col = this.col;
 		x = row * 80;
@@ -30,6 +35,71 @@ public class Space {
 		g.drawString("Space", x, y);	
 
 		System.out.println(row, " ", col);
+	}
+
+	public Space adjNorth() {
+		if (this.row !=0) {
+			Space update = new Space(this.col, this.row-1);
+			return update;
+		} else {
+			return null;
+		}
+		
+	}
+
+	public Space adjNorthEast() {
+		if (this.row == 0 || this.col==7) {
+			return null;
+		}
+		Space update = new Space(this.col+1, this.row-1);
+		return update;
+	}
+
+	public Space adjEast() {
+		if (this.col==7) {
+			return null;
+		}
+		Space update = new Space(this.col+1, this.row);
+		return update;
+	}
+
+	public Space adjSouthEast() {
+		if (this.row==7 || this.col==0) {
+			return null;
+		}
+		Space update = new Space(this.col+1, this.row+1);
+		return update;
+	}
+
+	public Space adjSouth() {
+		if (this.row==7) {
+			return null;
+		}
+		Space update = new Space(this.col, this.row+1);
+		return update;
+	}
+
+	public Space adjSouthWest() {
+		if (this.row ==7 || this.col == 0) {
+			return null;
+		}
+		Space update = new Space(this.col-1, this.row+1);
+		return update;
+	}
+
+	public Space adjWest() {
+		if (this.col == 0) {
+			return null;
+		}
+		Space update = new Space(this.col-1, this.row);
+		return update;
+	}
+
+	public Space adjNorthWest() {
+		if (this.row == 0 || this.col == 0) {
+			return null;
+		}
+		Space update = new Space(this.col-1, this.row-1);
 	}
 
 }

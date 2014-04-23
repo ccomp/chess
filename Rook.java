c7a843aa3f78fea82a3148f82f59f315299e3a6f
Cyron <cyron.completo@gmail.com>
2014-04-23 14:45:26 -0600

    mah nigguh Bill

--- Rook.java
+++ Rook.java
@@ -1,5 +1,22 @@
 import java.util.ArrayList;
 
-public class Rook extends piece {
+public class Rook extends Piece {
 	
+	public Rook(Space s, Boolean isBlack) {
+		super(s, isBlack);
+	}
+
+	public ArrayList<Space> getMoveLocations() {
+		ArrayList<Space> locs = new ArrayList<Space>();
+
+		if (super.getSpace().adjSouth().getPiece() == null) {
+			locs.add(super.getSpace().adjSouth());
+		}
+		if (super.getSpace().adjNorth().getPiece() == null) {
+			locs.add(super.getSpace().adjNorth());
+		}
+		// if (super.getSpace().adj)
+		// 
+		return locs;
+	}
 }

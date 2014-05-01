3f47f284d7a91d27a063967bafe7f3773e350959
Cyron <cyron.completo@gmail.com>
2014-05-01 09:19:11 -0600

    THIS ISN'T OGRE YET

--- Bishop.java
+++ Bishop.java
@@ -6,11 +6,16 @@ public class Bishop extends Piece {
 		super(s, isBlack);
 	}
 
-	// public ArrayList<Space> getMoveLocations() {
-	// 	ArrayList<Space> locs = new ArrayList<Space>();
+	public ArrayList<Space> getMoveLocations() {
+		ArrayList<Space> locs = new ArrayList<Space>();
+		Space here = super.getSpace();
 
-	// 	for (; ; ) {
-			
-	// 	}
-	// }
+		for (int i = 0; i<=315; i+=45) {
+			while (here.adjDeg(i).getPiece() == null || here.adjDeg(i).getPiece().isBlack() != isBlack) {
+				locs.add(here.adjDeg(i));
+			}
+		}
+
+		return locs;
+	}
 }

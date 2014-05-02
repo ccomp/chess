3c697a0027e812bdd0ee98aab1ccd60a85bd3a36
apcs <apcs@mvhs-imacu52828.dcsdk12.org>
2014-05-02 09:11:24 -0600

    stuff

--- King.java
+++ King.java
@@ -26,23 +26,50 @@ public class King extends Piece {
 
 		if (isBlack()) {
 
-			if (here.isEqual(bcl1) == true) {
-				locs.add(bcl1);
-			}
+			Space binit = new Space(4,7);
+			Space brs1 = new Space(0,0);
+			Space brs2 = new Space(7,0);
 
-			if (here.isEqual(bcl2) == true) {
-				locs.add(bcl2);
-			}				
+			if (getSpace.isEqual(binit)) {
+
+				if (pieces.indexOf(Rook bRook1) != -1) {
+					brIndex = pieces.indexOf(Rook bRook1);
+					if (brs1.isEqual(pieces.get(brIndex).getSpace())) {
+						locs.add(bcl1);
+					}				
+				}
+
+				if (pieces.indexOf(Rook bRook2) != -1) {
+					brIndex2 = pieces.indexOf(Rook bRook2);
+					if (brs2.isEqual(pieces.get(brIndex2).getSpace())) {
+						locs.add(bcl2);
+					}
+				}
+			}		
 		}
 
 		if (isBlack() == false) {
-			if (here.isEqual(wcl1)) {
-				locs.add(wcl1);
-			}
 
-			if (here.isEqual(wcl2)) {
-				locs.add(wcl2);
-			}
+			Space winit = new Space(4,7);
+			Space wrs1 = new Space(0,7);
+			Space wrs2 = new Space(7,7);
+
+			if (getSpace.isEqual(winit)) {
+
+				if (pieces.indexOf(Rook wRook1) != -1) {
+					wrIndex = pieces.indexOf(Rook wRook1);
+					if (wrs1.isEqual(pieces.get(wrIndex).getSpace())) {
+						locs.add(wcl1);
+					}				
+				}
+
+				if (pieces.indexOf(Rook wRook2) != -1) {
+					wrIndex2 = pieces.indexOf(Rook wRook2);
+					if (wrs2.isEqual(pieces.get(wrIndex2).getSpace())) {
+						locs.add(wcl2);
+					}
+				}
+			}		
 		}
 	return locs;
 	}
